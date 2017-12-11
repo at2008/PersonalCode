b23089bade8d8efd42730d82dfec0a2e8390bbde
at2008 <zhuo.cn@163.com>
2017-12-11 16:25:43 +0800

    优化判断旧文件夹重复的流程，简化代码。

--- SampleCode/SampleCode/DelLegacyFolder.cs
+++ SampleCode/SampleCode/DelLegacyFolder.cs
@@ -26,110 +26,56 @@ namespace SampleCode
                     string[] OlderFolderName = dirs[i].Split(',');
                     string[] NewerFolderName = dirs[i + 1].Split(',');
 
-                    if ((OlderFolderName[0] == NewerFolderName[0]) && (OlderFolderName.Length == NewerFolderName.Length))
+                    if ((OlderFolderName[0] == NewerFolderName[0])
+                        && (OlderFolderName[1] != NewerFolderName[1])
+                        && (OlderFolderName.Length == NewerFolderName.Length))
                     {
-                        if (OlderFolderName.Length == 2)
+                        bool ifNeedOperation = false;
+                        switch (OlderFolderName.Length)
                         {
-                            if ((OlderFolderName[0] == NewerFolderName[0])
-                            && (OlderFolderName[1] != NewerFolderName[1]))
-                            {
-                                string[] olderVersion = OlderFolderName[1].Substring(OlderFolderName[1].IndexOf("=") + 1).Split('.');
-                                string[] newerVersion = NewerFolderName[1].Substring(NewerFolderName[1].IndexOf("=") + 1).Split('.');
-                                for (int x = 0; x < olderVersion.Length; x++)
-                                {
-                                    if (Int32.Parse(olderVersion[x]) < Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
-                                    else if (Int32.Parse(olderVersion[x]) > Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i + 1]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
-                                }
-                            }
+                            case 2:
+                                ifNeedOperation = true;
+                                break;
+                            case 3:
+                                if ((OlderFolderName[2] == NewerFolderName[2]))
+                                    ifNeedOperation = true;
+                                break;
+                            case 4:
+                                if ((OlderFolderName[2] == NewerFolderName[2])
+                                    && (OlderFolderName[3] == NewerFolderName[3]))
+                                    ifNeedOperation = true;
+                                break;
+                            default:
+                                break;
                         }
-                        else if (OlderFolderName.Length == 3)
+
+                        if (ifNeedOperation)
                         {
-                            if ((OlderFolderName[0] == NewerFolderName[0])
-                            && (OlderFolderName[1] != NewerFolderName[1])
-                            && (OlderFolderName[2] == NewerFolderName[2]))
+                            string[] olderVersion = OlderFolderName[1].Substring(OlderFolderName[1].IndexOf("=") + 1).Split('.');
+                            string[] newerVersion = NewerFolderName[1].Substring(NewerFolderName[1].IndexOf("=") + 1).Split('.');
+                            for (int x = 0; x < olderVersion.Length; x++)
                             {
-                                string[] olderVersion = OlderFolderName[1].Substring(OlderFolderName[1].IndexOf("=") + 1).Split('.');
-                                string[] newerVersion = NewerFolderName[1].Substring(NewerFolderName[1].IndexOf("=") + 1).Split('.');
-                                for (int x = 0; x < olderVersion.Length; x++)
+                                if (Int32.Parse(olderVersion[x]) < Int32.Parse(newerVersion[x]))
                                 {
-                                    if (Int32.Parse(olderVersion[x]) < Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
-                                    else if (Int32.Parse(olderVersion[x]) > Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i + 1]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine(dirs[i]);
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine(dirs[i + 1]);
+                                    olderFolder.Add(dirs[i]);
+                                    count += 1;
+                                    i += 1;
+                                    break;
                                 }
-                            }
-                        }
-                        else if (OlderFolderName.Length == 4)
-                        {
-                            if ((OlderFolderName[0] == NewerFolderName[0])
-                            && (OlderFolderName[1] != NewerFolderName[1])
-                            && (OlderFolderName[2] == NewerFolderName[2])
-                            && (OlderFolderName[3] == NewerFolderName[3]))
-                            {
-                                string[] olderVersion = OlderFolderName[1].Substring(OlderFolderName[1].IndexOf("=") + 1).Split('.');
-                                string[] newerVersion = NewerFolderName[1].Substring(NewerFolderName[1].IndexOf("=") + 1).Split('.');
-                                for (int x = 0; x < olderVersion.Length; x++)
+                                else if (Int32.Parse(olderVersion[x]) > Int32.Parse(newerVersion[x]))
                                 {
-                                    if (Int32.Parse(olderVersion[x]) < Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
-                                    else if (Int32.Parse(olderVersion[x]) > Int32.Parse(newerVersion[x]))
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.White;
-                                        Console.WriteLine(dirs[i]);
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.WriteLine(dirs[i + 1]);
-                                        olderFolder.Add(dirs[i + 1]);
-                                        count += 1;
-                                        i += 1;
-                                        break;
-                                    }
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine(dirs[i]);
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine(dirs[i + 1]);
+                                    olderFolder.Add(dirs[i + 1]);
+                                    count += 1;
+                                    i += 1;
+                                    break;
                                 }
                             }
                         }
@@ -146,7 +92,6 @@ namespace SampleCode
                         Directory.Delete(olderFolder[i], true);
                     }
                 }
-
             }
             catch (UnauthorizedAccessException UAEx)
             {
