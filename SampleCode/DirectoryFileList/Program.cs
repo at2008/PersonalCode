f289f14fddcadf02dfb0c1cdff524ec49ce6b0c1
at2008 <zhuo.cn@163.com>
2017-12-21 10:25:17 +0800

    增加两个例子： DirectoryFileList FileCount

---
+++ SampleCode/DirectoryFileList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryFileList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length > 0)
+            {
+                directory = args[0];
+            }
+            else
+            {
+                directory = Directory.GetCurrentDirectory();
+            }
+
+            //directory = @"d:\1";
+
+            Console.WriteLine(directory);
+
+            DirectoryFileList(directory, directory);
+
+            Console.ReadKey();
+        }
+
+        static void DirectoryFileList(string beginDirectory, string currentDirectory)
+        {
+            int currentLevel = 0;
+            int level = currentDirectory.Split('\\').Length - beginDirectory.Split('\\').Length;
+
+            string formatPrefix = "|--";
+
+            while (currentLevel < level)
+            {
+                formatPrefix += "|--";
+                currentLevel++;
+            }
+
+            foreach (string subdirectory in Directory.GetDirectories(currentDirectory))
+            {
+                string[] currentPathName = subdirectory.Split('\\');
+                Console.WriteLine(formatPrefix + currentPathName[currentPathName.Length - 1]);
+
+                DirectoryFileList(beginDirectory, subdirectory);
+            }
+
+            foreach (string file in Directory.GetFiles(currentDirectory))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(formatPrefix + Path.GetFileName(file));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ SampleCode/FileCount/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace FileCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allFileCount = 0;
+            string directory;
+            if (args.Length > 0)
+            {
+                directory = args[0];
+            }
+            else
+            {
+                directory = Directory.GetCurrentDirectory();
+            }
+
+            allFileCount = DirectoryCountFiles(directory);
+
+            Console.WriteLine(directory);
+            Console.WriteLine(allFileCount);
+            Console.ReadKey();
+        }
+
+        static int DirectoryCountFiles(string directory)
+        {
+            int fileCount = 0;
+
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                fileCount++;
+            }
+
+            foreach (string subdirectory in Directory.GetDirectories(directory))
+            {
+                fileCount += DirectoryCountFiles(subdirectory);
+            }
+
+            return fileCount;
+        }
+
+
+    }
+}
